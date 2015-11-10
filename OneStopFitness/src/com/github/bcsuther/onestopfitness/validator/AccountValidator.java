c2b2be26430d91aaa0dbba5d7f8d2be2572d1e94
Brian Suther <briansuther5@gmail.com>
2015-11-10 16:42:06 -0600

    Added validation to see if username already exist before creating new account

--- OneStopFitness/src/com/github/bcsuther/onestopfitness/validator/AccountValidator.java
+++ OneStopFitness/src/com/github/bcsuther/onestopfitness/validator/AccountValidator.java
@@ -1,17 +1,23 @@
 package com.github.bcsuther.onestopfitness.validator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
+import com.github.bcsuther.onestopfitness.dao.AccountDao;
 import com.github.bcsuther.onestopfitness.model.UserProfile;
 
 @Component
 @Qualifier("accountValidator")
 public class AccountValidator implements Validator {
 
+	@Autowired
+	@Qualifier("accountDao")
+	AccountDao accountDao;
+	
 	@Override
 	public boolean supports(Class<?> clazz) {
 		return UserProfile.class.equals(clazz);
@@ -20,8 +26,15 @@ public class AccountValidator implements Validator {
 	@Override
 	public void validate(Object target, Errors errors) {
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstName", "error.firstName", "First name is required.");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "error.firstName", "Last name is required.");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "error.firstName", "username is required.");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "error.firstName", "Email is required.");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "error.lastName", "Last name is required.");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "error.username", "Username is required.");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "error.email", "Email is required.");
+		
+		UserProfile userProfile = (UserProfile) target;
+		UserProfile existingUser = accountDao.findUserByUsername(userProfile.getUsername());
+		if(existingUser.getUsername() != null && !existingUser.getUsername().isEmpty()) {
+			errors.rejectValue("username", "error.username", "Username already exist. Please choose a different one.");
+		}
+		
 	}
 }
